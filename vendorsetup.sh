baff07599530e77e065e0113f821bf6d573128b6
rk134 <spaced@rk134.cf>
2022-11-14 19:12:13 +0000

    spaced: completely rework vendorsetup script
    Signed-off-by: rk134 <spaced@rk134.cf>

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,25 +1,32 @@
 # Vendor tree
-VT=vendor/realme/spaced/spaced-vendor.mk
-if ! [ -a $VT ]; then git clone https://github.com/drtsinx92/proprietary_vendor_realme_spaced vendor/realme/spaced
-fi
+git clone https://github.com/drtsinx92/proprietary_vendor_realme_spaced vendor/realme/spaced
 
 # Vendor-ims tree
-VT_IMS=vendor/realme/ims-spaced/mtk-ims.mk
-if ! [ -a $VT_IMS ]; then git clone https://github.com/drtsinx92/proprietary_vendor_realme_ims-spaced vendor/realme/ims-spaced
+git clone https://github.com/drtsinx92/proprietary_vendor_realme_ims-spaced vendor/realme/ims-spaced
 
 # Kernel tree
-KT=kernel/realme/spaced/build.sh
-if ! [ -a $KT ]; then git clone --depth=1 https://github.com/drtsinx92/android_kernel_realme_mt6781 kernel/realme/spaced
-fi
+git clone --depth=1 https://github.com/drtsinx92/android_kernel_realme_mt6781 kernel/realme/spaced
 
 # frameworks/av (required for sysbta)
 cd frameworks/av
-BRANCH="$(git rev-parse --abbrev-ref HEAD)"
-if ! [ "$BRANCH" != "13-sysbta"]; then cd ../.. && git clone --depth=1 https://github.com/rk134/frameworks_av.git frameworks/av
-fi
+git fetch https://github.com/rk134/frameworks_av.git
+git cherry-pick 9c71f9a66d8b71d9bf1975f46c43c562d38dfc64
+git cherry-pick 9c71f9a66d8b71d9bf1975f46c43c562d38dfc64..1f16358102caed113917071d8b58cc9d1307855b
+cd ../..
+
 
 # packages/modules/Bluetooth (required for sysbta)
 cd packages/modules/Bluetooth
-BRANCH_BT="$(git rev-parse --abbrev-ref HEAD)"
-if ! [ "$BRANCH_BT" != "13-sysbta"]; then cd ../../.. && git clone --depth=1 https://github.com/rk134/packages_modules_Bluetooth.git packages/modules/Bluetooth
-fi
+git fetch https://github.com/rk134/packages_modules_Bluetooth.git
+git cherry-pick 78ec0b935982f22e8b2225af4e5e5f374f643d97
+git cherry-pick 50286b0eeaaa699cf5770a9cef24b35ec9f9aff4
+git cherry-pick 5610e46a47583df0fa6f731dd3196fbab158a2cd
+git cherry-pick b6d4693925823facd321a8a1d8ef7882f63fcc3c
+git cherry-pick 5ab4518edfc2299a4c2e395420981fbb669b8f90
+
+# Clang
+cd prebuilts/clang/host/
+git clone https://android.googlesource.com/platform/prebuilts/clang/host/linux-x86 clang -b android-12.1.0_r27 --depth=1
+mv clang/clang-r383902 linux-x86/clang-r383902
+rm -rf clang
+cd ../../..
